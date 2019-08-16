f581085a20f3e6422a44a122dca6fb644b06b2cb
宋梦瑶 <3076117417@qq.com>
2019-08-16 21:58:54 +0800

    webpack 管理输出

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
 import Data from './data.xml'
+import printMe from './print.js'
 
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   // Lodash，现在由此脚本导入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
@@ -16,6 +18,10 @@ function component() {
   element.appendChild(myIcon)
 
   console.log('Data: ', Data)
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   // 入口文件
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    // build 前清空dist文件夹
+    new CleanWebpackPlugin(),
+    // 生成dist文件夹里面的index.html文件
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   // 输出文件
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
